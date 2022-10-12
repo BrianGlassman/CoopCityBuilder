b5e5a37feb1715e30a2dd785228340d11edd2c97
Brian Glassman <15279030+BrianGlassman@users.noreply.github.com>
2022-10-12 13:02:52 -0600

    split out HexCoordinates

--- Assets/TempWork/Brian/Hex/HexGrid.cs
+++ Assets/TempWork/Brian/Hex/HexGrid.cs
@@ -49,47 +49,6 @@ public class HexGrid : MonoBehaviour
     //----------------------
     */
 
-    /// <summary>
-    /// A class for storing hex coordinates and converting between other coordinate systems
-    /// </summary>
-    [System.Serializable]
-    private struct HexCoordinates
-    {
-        // Coordinates are immutable once created because the properties are read-only
-        public int H { get; private set; }
-        public int D { get; private set; }
-
-        /*
-         * Constructors
-         */
-        /// <summary>
-        /// Create a HexCoordinates object using our coordinate system.
-        /// </summary>
-        /// <param name="H">Horizontal measurement, increases moving to the right</param>
-        /// <param name="D">Diagonal measurement, increases moving to the top-left</param>
-        public HexCoordinates(int H, int D)
-        {
-            this.H = H;
-            this.D = D;
-        }
-        /// <summary>
-        /// Converts the Axial system from Redblog Games to the system we're using.
-        /// https://www.redblobgames.com/grids/hexagons/#coordinates
-        /// </summary>
-        /// <param name="q">Horizontal measurement, increases moving to the right</param>
-        /// <param name="r">Diagonal measurement, increases moving to the bottom-right</param>
-        /// <returns></returns>
-        public static HexCoordinates FromRedblogAxialCoordinates(int q, int r)
-        {
-            return new HexCoordinates(q, -r);
-        }
-
-        public override string ToString()
-        {
-            return "(" + H.ToString() + ", " + D.ToString() + ")";
-        }
-    }
-
     public static int width = 6;
     public static int height = 6;
 
