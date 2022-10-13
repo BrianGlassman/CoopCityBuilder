c224ca7648d6cb846a11e4b87643dd3e41cea43f
Brian Glassman <15279030+BrianGlassman@users.noreply.github.com>
2022-10-13 16:02:25 -0600

    prefix Unity-like interface

--- Assets/TempWork/Brian/Hex/HexGrid.cs
+++ Assets/TempWork/Brian/Hex/HexGrid.cs
@@ -18,7 +18,7 @@ public class HexGrid : MonoBehaviour
     /// </summary>
     /// <param name="position">The cell coordinates</param>
     /// <returns></returns>
-    public Vector3 GetCellCenterLocal(Vector3Int position)
+    public Vector3 Unity_GetCellCenterLocal(Vector3Int position)
     {
         var coords = HexCoordinates.FromVector3Int(position);
         return HexMetrics.CellToLocal(coords);
@@ -28,11 +28,11 @@ public class HexGrid : MonoBehaviour
     /// </summary>
     /// <param name="position">The cell coordinates</param>
     /// <returns></returns>
-    public Vector3 GetCellCenterWorld(Vector3Int position)
+    public Vector3 Unity_GetCellCenterWorld(Vector3Int position)
     {
         var coords = HexCoordinates.FromVector3Int(position);
         var local = HexMetrics.CellToLocal(coords);
-        return LocalToWorld(local);
+        return Unity_LocalToWorld(local);
     }
     // ##### Grid interface - static methods #####
     // TODO public static Vector3 InverseSwizzle(GridLayout.CellSwizzle swizzle, Vector3 position);
@@ -42,20 +42,20 @@ public class HexGrid : MonoBehaviour
     /// The size of the gap between each cell in the Grid.
     /// Only non-zero for isometric grids.
     /// </summary>
-    public Vector3 cellGap = Vector3.zero;
+    public Vector3 Unity_cellGap = Vector3.zero;
     /// <summary>
     /// The layout of the GridLayout.
     /// The layout determines the conversion of positions from cell space to local space and vice versa.
     /// </summary>
-    public GridLayout.CellLayout cellLayout = GridLayout.CellLayout.Hexagon;
+    public GridLayout.CellLayout Unity_cellLayout = GridLayout.CellLayout.Hexagon;
     /// <summary>
     /// The size of each cell in the Grid
     /// </summary>
-    public Vector3 cellSize { get { return HexMetrics.cellSize; } }
+    public Vector3 Unity_cellSize { get { return HexMetrics.cellSize; } }
     /// <summary>
     /// The cell swizzle for the grid
     /// </summary>
-    public GridLayout.CellSwizzle cellSwizzle()
+    public GridLayout.CellSwizzle Unity_cellSwizzle()
     {
         return GridLayout.CellSwizzle.XZY;
     }
@@ -66,13 +66,13 @@ public class HexGrid : MonoBehaviour
     /// </summary>
     /// <param name="cellPosition">The cell coordinates</param>
     /// <returns></returns>
-    public Vector3 CellToLocal(Vector3Int cellPosition) { return GetCellCenterLocal(cellPosition); }
+    public Vector3 Unity_CellToLocal(Vector3Int cellPosition) { return Unity_GetCellCenterLocal(cellPosition); }
     /// <summary>
     /// Converts an interpolated cell position in floats to local position space.
     /// </summary>
     /// <param name="cellPosition">Interpolated cell position to convert</param>
     /// <returns></returns>
-    public Vector3 CellToLocalInterpolated(Vector3 cellPosition)
+    public Vector3 Unity_CellToLocalInterpolated(Vector3 cellPosition)
     {
         // FIXMELOW Not future-proofed against changes in [Vector3 --> cell coordinates] conversion
         return HexMetrics.CellToLocalInterpolated(cellPosition[0], cellPosition[1]);
@@ -83,36 +83,36 @@ public class HexGrid : MonoBehaviour
     /// </summary>
     /// <param name="cellPosition">The cell coordinates</param>
     /// <returns></returns>
-    public Vector3 CellToWorld(Vector3Int cellPosition) { return GetCellCenterWorld(cellPosition); }
+    public Vector3 Unity_CellToWorld(Vector3Int cellPosition) { return Unity_GetCellCenterWorld(cellPosition); }
     // TODO public Bounds GetBoundsLocal(Vector3Int cellPosition);
     // TODO public Vector3 GetLayoutCellCenter();
-    public Vector3Int LocalToCell(Vector3 localPosition)
+    public Vector3Int Unity_LocalToCell(Vector3 localPosition)
     {
         // FIXMELOW Not future-proofed against changes in [Vector3 --> cell coordinates] conversion
         var hd = HexMetrics.LocalToCell(localPosition);
         return new Vector3Int(hd[0], 0, hd[1]);
     }
-    public Vector3 LocalToCellInterpolated(Vector3 localPosition)
+    public Vector3 Unity_LocalToCellInterpolated(Vector3 localPosition)
     {
         // FIXMELOW Not future-proofed against changes in [Vector3 --> cell coordinates] conversion
         var hd = HexMetrics.LocalToCellInterpolated(localPosition);
         return new Vector3(hd[0], 0, hd[1]);
     }
-    public Vector3 LocalToWorld(Vector3 localPosition)
+    public Vector3 Unity_LocalToWorld(Vector3 localPosition)
     {
         return transform.TransformPoint(localPosition);
     }
-    public Vector3Int WorldToCell(Vector3 worldPosition)
+    public Vector3Int Unity_WorldToCell(Vector3 worldPosition)
     {
-        var localPos = WorldToLocal(worldPosition);
-        return LocalToCell(localPos);
+        var localPos = Unity_WorldToLocal(worldPosition);
+        return Unity_LocalToCell(localPos);
     }
     /// <summary>
     /// Converts a world position to local position.
     /// </summary>
     /// <param name="worldPosition">World Position to convert.</param>
     /// <returns></returns>
-    public Vector3 WorldToLocal(Vector3 worldPosition) { return transform.InverseTransformPoint(worldPosition); }
+    public Vector3 Unity_WorldToLocal(Vector3 worldPosition) { return transform.InverseTransformPoint(worldPosition); }
     //----------------------
     
     // Alternate input for Unity-like function
@@ -166,6 +166,7 @@ public class HexGrid : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        // FIXME use non-Unity interface
         Gizmos.color = Color.white;
         for (int d = 0; d < height; d++)
         {
@@ -175,15 +176,15 @@ public class HexGrid : MonoBehaviour
                 var localPos = HexMetrics.CellToLocal(h, d);
                 // Label the coordinates
                 UnityEditor.Handles.Label(
-                    LocalToWorld(localPos),
+                    Unity_LocalToWorld(localPos),
                     "(" + h.ToString() + ", " + d.ToString() + ")"
                 );
                 // Draw the hex outline
                 for (int i = 0; i < 6; i++)
                 {
                     Gizmos.DrawLine(
-                        LocalToWorld(HexMetrics.corners[i] + localPos),
-                        LocalToWorld(HexMetrics.corners[i + 1] + localPos)
+                        Unity_LocalToWorld(HexMetrics.corners[i] + localPos),
+                        Unity_LocalToWorld(HexMetrics.corners[i + 1] + localPos)
                     );
                 }
             }
